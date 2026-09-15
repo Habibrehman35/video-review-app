259fb2872b3942ca8a4bd7c054eb62678bea0f2b
habibrehman35 <hassebideas35@gmail.com>
2026-09-15 09:12:40 +0500

    remove set/remove cookies from server component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -33,6 +33,7 @@ interface Testimonial {
 export default async function DashboardPage() {
   const cookieStore = await cookies()
   
+  // Server Component mein sirf 'get' use hota hai, set/remove nahi!
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -40,16 +41,6 @@ export default async function DashboardPage() {
       cookies: {
         get(name: string) {
           return cookieStore.get(name)?.value
-        },
-        set(name: string, value: string, options: any) {
-          try {
-            cookieStore.set({ name, value, ...options })
-          } catch {}
-        },
-        remove(name: string, options: any) {
-          try {
-            cookieStore.set({ name, value: '', ...options })
-          } catch {}
         },
       },
     }
